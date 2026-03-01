5dc6d1e3fdd32f38eec28b611940769d680efbe5
Sreeya C <sreeyachand@gmail.com>
2026-03-02 00:52:51 +0530

    Change units to 'metric'

--- tests/test-dpapi.py
+++ tests/test-dpapi.py
@@ -1,4 +1,4 @@
 from noaa_coops import Station
 s= Station(1611400)
-df = s.get_derived_product("toptenwaterlevels", station_id="1611400", datum="MHHW", units="english")
+df = s.get_derived_product("toptenwaterlevels", station_id="1611400", datum="MHHW", units="metric")
 print(df)
